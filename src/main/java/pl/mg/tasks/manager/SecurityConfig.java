67da2a7f131d5ff8f56fcc7356a62c74ded01a9b
Marcin <marcin.grzmowicz94@gmail.com>
2020-03-11 16:54:33 +0100

    Added basic Security Configurations.

---
+++ src/main/java/pl/mg/tasks/manager/SecurityConfig.java
@@ -0,0 +1,33 @@
+package pl.mg.tasks.manager;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user1").password("user1").authorities("ROLE_USER")
+                .and().withUser("user2").password("user2").authorities("ROLE_USER")
+                .and().withUser("admin").password("admin").authorities("ROLE_ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/tasks", "/tasks/*").permitAll()
+                .antMatchers(HttpMethod.POST, "/tasks", "/tasks/*").hasRole("USER")
+                .antMatchers(HttpMethod.DELETE, "/tasks", "/tasks/*").hasRole("ADMIN")
+                .antMatchers(HttpMethod.PUT, "/tasks", "/tasks/*").hasRole("ADMIN")
+                .and().csrf().disable().httpBasic();
+    }
+}
